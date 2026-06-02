b39a506e8986c69c899dce8c5d8ad84b4e32e0cd
Bethel03 <127938599+Bethel03@users.noreply.github.com>
2026-06-02 21:36:58 +0200

    FIX: Update a todo route implementation

--- server/server.js
+++ server/server.js
@@ -53,6 +53,19 @@ app.get('/todos/:id', async(req, res) =>{
 
 //Update a todo
 
+app.put('/todos/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const {description} = req.body;
+        const upadateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2",
+            [description, id]
+        );
+        res.json("Todo was updated!")
+    } catch (err) {
+        console.error(err.message);
+    }
+});
+
 //Delete a todo
 
 app.listen(5000, () => {
