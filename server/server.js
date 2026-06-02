7f7e3317165e229cb422326e3d5940f9e3e85183
Bethel03 <127938599+Bethel03@users.noreply.github.com>
2026-06-02 14:55:11 +0200

    FIX: Create a todo query and result

--- server/server.js
+++ server/server.js
@@ -15,15 +15,15 @@ app.use(express.json());
 app.post('/todos', async(req, res) => {
     try {
         const {description} = req.body;
-        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1)",
+        const newTodo = await pool.query("INSERT INTO todo (description) VALUES($1) RETURNING *",
             [description]
         );
 
-        res.json(newTodo);
+        res.json(newTodo.rows[0]);
     } catch (err) {
         console.log(err.message);
     }
-})
+});
 //Get all todos
 
 //Get a todo
