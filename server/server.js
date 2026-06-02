273f021d3e4ed55106a80016cc63803e88cbf539
Bethel03 <127938599+Bethel03@users.noreply.github.com>
2026-06-02 15:18:08 +0200

    FIX: Get a todo route implementation

--- server/server.js
+++ server/server.js
@@ -34,9 +34,22 @@ app.get('/todos', async(req, res) =>{
     } catch (err) {
         console.log(err.message);
     }
-})
+});
 
 //Get a todo
+
+app.get('/todos/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", 
+            [id]
+        );
+
+        res.json(todo.rows[0]);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
 
 //Update a todo
 
