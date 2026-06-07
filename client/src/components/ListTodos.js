363f1362956ad3601328a88cacbace755a35cfe2
Bethel03 <127938599+Bethel03@users.noreply.github.com>
2026-06-07 22:37:40 +0200

    FIX: Delete todo function in ListTodos

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -4,6 +4,7 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    // Get the todos
     const getTodos = async () => {
         try {
             const response = await fetch("http://localhost:5001/todos");
@@ -19,6 +20,22 @@ const ListTodos = () => {
         getTodos();
     }, []);
 
+    // Delete a todo
+
+    const deleteTodo = async (id) =>{
+        try {
+            const deleteTodo = await fetch(`http://localhost:5001/todos/${id}`,
+                {
+                method: "DELETE"
+            });
+            
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
     return (
         <Fragment>
             <table className="table mt-5 text-center">
@@ -31,10 +48,13 @@ const ListTodos = () => {
                 </thead>
                 {todos.map(todo => (
                     <tbody>
-                        <tr>
+                        <tr key={todo.todo_id}>
                             <td>{todo.description}</td>
                             <td>Edit</td>
-                            <td>Delete</td>
+                            <td>
+                                <button className="btn btn-danger" 
+                                onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
+                                </td>
                         </tr>
                     </tbody>
                 ))}
